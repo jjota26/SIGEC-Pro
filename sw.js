076681e798a494205f941d64b5751f572617f5d5
josecenturio <josecenturio@users.noreply.huggingface.co>
2026-09-24 22:39:19 +0200

    Atualizar icones PWA para o logotipo oficial Centauro Sinergia

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Estrategia: Network First com fallback para cache
    ============================================================ */
 
-const CACHE_NAME = 'sigec-pro-v5.16';
+const CACHE_NAME = 'sigec-pro-v5.17';
 const CORE_ASSETS = [
   "/",
   "/index.html",
